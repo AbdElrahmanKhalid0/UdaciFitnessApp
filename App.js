e5c8dfa6fc883b21e8c82b9ed409c642012b336d
Abdelrahman Khalid <abdelrahman.elzohry2014@gmail.com>
2020-03-28 21:20:05 +0200

    Initial files of the project

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>This is me Abdelrahman</Text>
+      <Text>what about you??</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
