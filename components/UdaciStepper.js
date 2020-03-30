0681a42031d450e681cd004407ba20dade1d17eb
Abdelrahman Khalid <abdelrahman.elzohry2014@gmail.com>
2020-03-30 04:13:39 +0200

    added the steppers to the UdaciStepper component

--- components/UdaciStepper.js
+++ components/UdaciStepper.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import {View,Text} from 'react-native';
+import {View,Text,TouchableOpacity} from 'react-native';
+import Entypo from 'react-native-vector-icons/Entypo';
 
-export default function UdaciStepper () {
+export default function UdaciStepper ({value,onIncrement,onDecrement,unit}) {
     return (
         <View>
-            <Text>Stepper</Text>
+            <View>
+                <TouchableOpacity onPress={onDecrement}>
+                    <Entypo name='minus' size={30} color='black'/>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={onIncrement}>
+                    <Entypo name='plus' size={30} color='black'/>
+                </TouchableOpacity>
+            </View>
+            <View>
+                <Text>{value}</Text>
+                <Text>{unit}</Text>
+            </View>
         </View>
     )
 }
