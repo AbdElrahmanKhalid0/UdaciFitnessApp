387ab0ce3eda8f6fd318542823ee37d74dbde355
Abdelrahman Khalid <abdelrahman.elzohry2014@gmail.com>
2020-03-30 03:29:58 +0200

    touchable components in react native

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View,StyleSheet,Text, TouchableHighlight, TouchableOpacity, TouchableWithoutFeedback, TouchableNativeFeedback } from 'react-native';
 import AddEntry from './components/AddEntry';
 
 export default function App() {
   return (
-    <View>
-      <AddEntry/>
+    <View style={styles.container}>
+      {/* <AddEntry/> */}
+      <TouchableNativeFeedback onPress={() => {console.log('this is working')}}>
+        <View style={styles.btn}>
+          <Text>hola niggas</Text>
+        </View>
+      </TouchableNativeFeedback>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    marginLeft:10,
+    marginTop:10,
+    justifyContent:'center',
+    alignItems:"center"
+  },
+  btn:{
+    backgroundColor:'lightgreen',
+    padding:10,
+    borderRadius:20,
+    width:150,
+    alignItems:'center'
+  }
+})
