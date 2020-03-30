db0a7f58e7bd34a9f2594a443a0857579df87902
Abdelrahman Khalid <abdelrahman.elzohry2014@gmail.com>
2020-03-30 03:30:43 +0200

    got the application to its original state

--- App.js
+++ App.js
@@ -1,33 +1,11 @@
 import React from 'react';
-import { View,StyleSheet,Text, TouchableHighlight, TouchableOpacity, TouchableWithoutFeedback, TouchableNativeFeedback } from 'react-native';
+import { View } from 'react-native';
 import AddEntry from './components/AddEntry';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <AddEntry/> */}
-      <TouchableNativeFeedback onPress={() => {console.log('this is working')}}>
-        <View style={styles.btn}>
-          <Text>hola niggas</Text>
-        </View>
-      </TouchableNativeFeedback>
+    <View>
+      <AddEntry/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    marginLeft:10,
-    marginTop:10,
-    justifyContent:'center',
-    alignItems:"center"
-  },
-  btn:{
-    backgroundColor:'lightgreen',
-    padding:10,
-    borderRadius:20,
-    width:150,
-    alignItems:'center'
-  }
-})
