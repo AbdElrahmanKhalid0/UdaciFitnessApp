a6b10b4bee3304ea8aa38f4533301e6654f6c142
Abdelrahman Khalid <abdelrahman.elzohry2014@gmail.com>
2020-04-05 07:35:26 +0200

    added the views for the undetermined and denied cases for the location premission

--- components/Live.js
+++ components/Live.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { View,Text,ActivityIndicator } from 'react-native';
+import { View,Text,ActivityIndicator,StyleSheet,Platform,TouchableOpacity } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { purple, white } from '../utils/colors';
 
 export default class Live extends Component{
     state={
         coords:null,
-        status:null,
+        status:'denied',
         direction:'',
+    }
+    askPermission = () => {
+
     }
     render(){
         const {coords,status,direction} = this.state;
@@ -18,16 +23,24 @@ export default class Live extends Component{
 
         if(status === 'denied'){
             return (
-                <View>
-                    <Text>Denied</Text>
+                <View style={styles.center}>
+                    <Ionicons style={{margin:10}} name={Platform.OS === 'ios' ? 'ios-warning' : 'md-warning'} size={50} color={purple}/>
+                    <Text style={{textAlign:'center'}}>
+                        You denied to give the application the location permission. You can fix this by visiting
+                        your settings and enabling location services for this applicaiton.
+                    </Text>
                 </View>
             )
         };
 
         if(status === 'undetermined'){
             return (
-                <View>
-                    <Text>Undetermined</Text>
+                <View style={styles.center}>
+                    <Ionicons style={{margin:10}} name={Platform.OS === 'ios' ? 'ios-warning' : 'md-warning'} size={50} color={purple}/>
+                    <Text>You need To give us permission to use the App.</Text>
+                    <TouchableOpacity style={styles.button} onPress={this.askPermission}>
+                        <Text style={styles.buttonText}>Enable</Text>
+                    </TouchableOpacity>
                 </View>
             )
         };
@@ -40,3 +53,25 @@ export default class Live extends Component{
         )
     }
 }
+
+const styles = StyleSheet.create({
+    center:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        padding:15,
+    },
+    button:{
+        backgroundColor:purple,
+        width:100,
+        height:50,
+        justifyContent:'center',
+        alignItems:'center',
+        margin:10,
+        borderRadius: Platform.OS === 'ios' ? 8 : 3,
+    },
+    buttonText:{
+        fontSize:20,
+        color:white,
+    }
+});
