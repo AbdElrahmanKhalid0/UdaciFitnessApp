357c077a2a23191df4bee28bd9d9e3358f22a6bc
Abdelrahman Khalid <abdelrahman.elzohry2014@gmail.com>
2020-04-05 13:18:42 +0200

    added the live Location functionality to the view and implemented the geolocation native feature

--- components/Live.js
+++ components/Live.js
@@ -2,28 +2,76 @@ import React, { Component } from 'react';
 import { View,Text,ActivityIndicator,StyleSheet,Platform,TouchableOpacity } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { purple, white } from '../utils/colors';
+import * as Permissions from 'expo-permissions';
+import * as Location from 'expo-location';
+import { calculateDirection } from '../utils/helpers';
 
 export default class Live extends Component{
     state={
         coords:null,
-        status:'success',
+        status:null,
         direction:'',
     }
+    componentDidMount(){
+        this.askPermission(); // this asks for the Location permission once the component mounts
+        Permissions.getAsync(Permissions.LOCATION)
+            .then(({status}) => {
+                if (status === 'granted') {
+                    this.setState({status})
+                    return this.setLocation() // if there is a LOCAITON permission it will call the setLocation func
+                }
+                // other than that it will just update the status in the state
+                this.setState({status})
+            })
+            .catch(err => {
+                this.setState({status:'undetermined'});
+                console.warn('There was an error in getting the Location Permission: ', err);
+            })
+    }
     askPermission = () => {
+        Permissions.askAsync(Permissions.LOCATION)
+            .then(({status}) => {
+                if (status === 'granted') {
+                    this.setState({status})
+                    return this.setLocation() // if there is a LOCAITON permission it will call the setLocation func
+                }
+                // other than that it will just update the status in the state
+                this.setState({status})
+            })
+            .catch(err => {
+                this.setState({status:'undetermined'});
+                console.warn('There was an error in asking the Location Permission: ', err);
+            })
+    }
+    setLocation = () => {
+        Location.watchPositionAsync({
+            accuracy:Location.Accuracy.BestForNavigation,
+            timeInterval:1, // in milliseconds
+            distanceInterval:1, // in meters
+        },({coords}) => {
+            const newDirection = calculateDirection(coords.heading);
 
+            this.setState({
+                coords,
+                status:'granted',
+                direction:newDirection,
+            })
+        })
     }
     render(){
         const {coords,status,direction} = this.state;
 
         if(status === null){
             return (
-                <ActivityIndicator style={{marginTop:30}}/>
+                <View style={[styles.center,{flex:1}]}>
+                    <ActivityIndicator size={50}/>
+                </View>
             )
         };
 
         if(status === 'denied'){
             return (
-                <View style={styles.center}>
+                <View style={[styles.center,{flex:1}]}>
                     <Ionicons style={{margin:10}} name={Platform.OS === 'ios' ? 'ios-warning' : 'md-warning'} size={50} color={purple}/>
                     <Text style={{textAlign:'center'}}>
                         You denied to give the application the location permission. You can fix this by visiting
@@ -35,7 +83,7 @@ export default class Live extends Component{
 
         if(status === 'undetermined'){
             return (
-                <View style={styles.center}>
+                <View style={[styles.center,{flex:1}]}>
                     <Ionicons style={{margin:10}} name={Platform.OS === 'ios' ? 'ios-warning' : 'md-warning'} size={50} color={purple}/>
                     <Text>You need To give us permission to use the App.</Text>
                     <TouchableOpacity style={styles.button} onPress={this.askPermission}>
@@ -45,20 +93,28 @@ export default class Live extends Component{
             )
         };
 
+        if(!coords) {
+            return (
+                <View style={[styles.center,{flex:1}]}>
+                    <ActivityIndicator size={50}/>
+                </View>
+            )
+        }
+
         return (
             <View style={styles.container}>
                 <View style={[styles.center,{flex:1}]}>
                     <Text style={styles.headingText}>You're heading</Text>
-                    <Text style={styles.direction}>North</Text>
+                    <Text style={styles.direction}>{direction}</Text>
                 </View>
                 <View style={[styles.center,styles.footer]}>
                     <View style={styles.subContainer}>
                         <Text style={[styles.headingText,{color:white}]}>Altitude</Text>
-                        <Text style={styles.metricText}>{200} Feet</Text>
+                        <Text style={styles.metricText}>{coords.altitude} Meters</Text>
                     </View>
                     <View style={styles.subContainer}>
                         <Text style={[styles.headingText,{color:white}]}>Speed</Text>
-                        <Text style={styles.metricText}>{300} MPH</Text>
+                        <Text style={styles.metricText}>{coords.speed.toFixed(1)} Meters/sec</Text>
                     </View>
                 </View>
             </View>
@@ -92,8 +148,9 @@ const styles = StyleSheet.create({
         padding:15,
     },
     direction:{
-        fontSize:60,
-        color:purple
+        fontSize:70,
+        color:purple,
+        textAlign:'center',
     },
     center:{
         justifyContent:'center',
